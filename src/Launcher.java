bd04ea25e22e111788a95c42276787926ffa8ba6
glasch <a.glasch@hotmail.com>
2019-04-09 08:47:09 +0400

    2

--- src/Launcher.java
+++ src/Launcher.java
@@ -18,7 +18,7 @@ public class Launcher {
 
         MainController mainController = new MainController();
         mainController.showGUI();
-        System.out.println("mainController = "+7);
+        System.out.println("mainController = "+8);
 
     }
 
